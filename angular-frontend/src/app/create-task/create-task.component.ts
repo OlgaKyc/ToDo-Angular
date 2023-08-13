ebb09737e9d0fbcf6a7501912994f457cd1779ae
OlgaKyc <olga.kycia@interia.pl>
2023-08-14 00:32:40 +0200

    ToDo frontend working

---
+++ angular-frontend/src/app/create-task/create-task.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Task } from '../task';
+import { TaskService } from '../task.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-task',
+  templateUrl: './create-task.component.html',
+  styleUrls: ['./create-task.component.css']
+})
+export class CreateTaskComponent {
+
+  task: Task = new Task();
+  constructor(private taskService: TaskService,
+    private router: Router) {}
+
+  saveTask(){
+    this.taskService.createTask(this.task).subscribe( data =>{
+      console.log(data);
+      this.goToTaskList();
+    },
+    error => console.log(error));
+  }
+
+  goToTaskList(){
+    this.router.navigate(['/tasks']);
+  }
+
+  onSubmit() {
+    console.log(this.task);
+    this.saveTask();
+  }
+
+}
---
+++ angular-frontend/src/app/task.ts
@@ -0,0 +1,7 @@
+export class Task {
+    id!: number;
+    description!: String;
+    due_date!: Date;
+    title!: String;
+    type!: String;
+}
